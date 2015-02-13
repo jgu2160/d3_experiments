42e278fa5baec70b593649463ff57039cebeff69
Jeffrey Gu <jeffwgu@gmail.com>
2015-02-12 23:58:51 -0700

    understand most of advanced bar charting

--- bar_3/js/d3_world.js
+++ bar_3/js/d3_world.js
@@ -1,38 +1,40 @@
-var data = [4, 8, 15, 16, 23, 42];
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var width = 420,
-    barHeight = 20;
-
-var x = d3.scale.linear()
-    .range([0, width]);
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+var y = d3.scale.linear()
+    .range([height, 0]);
 
 var chart = d3.select(".chart")
-    .attr("width", width)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("./data/names.tsv", type, function(error, data) {
-    x.domain([0, d3.max(data, function(d) { return d.value; })]);
+    d3.tsv("./data/bar_3.tsv", type, function(error, data) {
+  x.domain(data.map(function(d) { return d.letter; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-    chart.attr("height", barHeight * data.length);
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + x(d.letter) + ",0)"; });
 
-    var bar = chart.selectAll("g")
-        .data(data)
-      .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+  bar.append("rect")
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); })
+      .attr("width", x.rangeBand());
 
-    bar.append("rect")
-        .attr("width", function(d) { return x(d.value); })
-        .attr("height", barHeight - 1);
-
-    bar.append("text")
-        .attr("x", function(d) { return x(d.value) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".30em")
-        .text(function(d) { return d.name; });
+  bar.append("text")
+      .attr("x", x.rangeBand() / 2)
+      .attr("y", function(d) { return y(d.frequency) + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.letter; });
 });
 
 function type(d) {
-    d.value = +d.value;
-    return d;
+  d.frequency = +d.frequency; // coerce to number
+  return d;
 }
-
-
